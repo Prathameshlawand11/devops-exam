3dcad03067b384ff7329a6f39a2641ba73dd2242
Prathamesh Lawand <prathameshl@prescient.in>
2025-02-04 16:04:36 +0530

    update6

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-#public Subnet
+# Public Subnet
 resource "aws_subnet" "PublicSubnet" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id     = data.aws_vpc.vpc.id
   cidr_block = "10.0.100.0/24"
 
   tags = {
     Name = "PublicSubnet"
   }
 }
-#Private Subnet
+
+# Private Subnet
 resource "aws_subnet" "PrivateSubnet" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id     = data.aws_vpc.vpc.id
   cidr_block = "10.0.110.0/24"
 
   tags = {
@@ -17,86 +18,86 @@ resource "aws_subnet" "PrivateSubnet" {
   }
 }
 
-#Public route table 
+# Public route table
 resource "aws_route_table" "PublicRT" {
   vpc_id = data.aws_vpc.vpc.id
-  }
+}
 
-#Private route table 
+# Private route table
 resource "aws_route_table" "PrivateRT" {
   vpc_id = data.aws_vpc.vpc.id
-  }
+}
 
-# route table association for public
+# Route table association for public
 resource "aws_route_table_association" "PublicToPublic" {
   subnet_id      = aws_subnet.PublicSubnet.id
   route_table_id = aws_route_table.PublicRT.id
 }
 
-# route table association for private
+# Route table association for private
 resource "aws_route_table_association" "PrivateToPrivate" {
   subnet_id      = aws_subnet.PrivateSubnet.id
   route_table_id = aws_route_table.PrivateRT.id
 }
 
+# Route in Public RT to IGW (for Public Subnet)
 resource "aws_route" "RouteInPublicRT_TO_IGW" {
-  route_table_id            = aws_route_table.PublicRT.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                 = data.aws_nat_gateway.nat.id
-  depends_on                = [aws_route_table.PublicRT]
-
+  route_table_id         = aws_route_table.PublicRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = data.aws_internet_gateway.igw.id
 }
 
+# Route in Private RT to NAT GW (for Private Subnet)
 resource "aws_route" "RouteInPrivateRT_TO_NATGW" {
-  route_table_id            = aws_route_table.PrivateRT.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = data.aws_nat_gateway.nat.id
-  depends_on                = [aws_route_table.PrivateRT]
+  route_table_id         = aws_route_table.PrivateRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = data.aws_nat_gateway.nat.id
 }
 
-#security group
+# Security Group for Lambda
 resource "aws_security_group" "SG" {
   name        = "SG"
-  description = " SG to alllow traffic from the VPC"
-  vpc_id = data.aws_vpc.vpc.id
-  depends_on = [
-    data.aws_vpc.vpc
-  ]
+  description = "SG to allow traffic from the VPC"
+  vpc_id      = data.aws_vpc.vpc.id
 
- 
-#outbound access
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-#creating zip of lambda file
+# Creating zip of lambda file
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda.py"
   output_path = "lambda.zip"
 }
 
-#lambda function configuration
+# Lambda function configuration
 resource "aws_lambda_function" "lambda_handler" {
-  filename      = data.archive_file.lambda.output_path
-  function_name = "lambda_handler"
-  role          = data.aws_iam_role.lambda.arn
-  handler       = "lambda.lambda_handler"
-  runtime       = "python3.9"
+  filename         = data.archive_file.lambda.output_path
+  function_name    = "lambda_handler"
+  role             = data.aws_iam_role.lambda.arn
+  handler          = "lambda.lambda_handler"
+  runtime          = "python3.9"
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
+  vpc_config {
+    subnet_ids         = [aws_subnet.PrivateSubnet.id]
+    security_group_ids = [aws_security_group.SG.id]
+  }
 }
 
+# Output the Private Subnet ID
 output "subnet_id" {
   value = aws_subnet.PrivateSubnet.id
 }
-
-
-
-
-
