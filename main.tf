777bba7eb987561c5242b2bea6745a0f4e4d4d12
Prathamesh Lawand <prathameshl@prescient.in>
2025-02-04 15:29:47 +0530

    config

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-#public Subnet
+# Public Subnet
 resource "aws_subnet" "PublicSubnet" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id     = data.aws_vpc.vpc.id
   cidr_block = "10.0.100.0/24"
 
   tags = {
     Name = "PublicSubnet"
   }
 }
-#Private Subnet
+
+# Private Subnet
 resource "aws_subnet" "PrivateSubnet" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id     = data.aws_vpc.vpc.id
   cidr_block = "10.0.110.0/24"
 
   tags = {
@@ -17,86 +18,98 @@ resource "aws_subnet" "PrivateSubnet" {
   }
 }
 
-#Public route table 
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  tags = {
+    Name = "PublicIGW"
+  }
+}
+
+# Public Route Table
 resource "aws_route_table" "PublicRT" {
   vpc_id = data.aws_vpc.vpc.id
 
+  tags = {
+    Name = "PublicRT"
   }
+}
 
-#Private route table 
+# Private Route Table
 resource "aws_route_table" "PrivateRT" {
   vpc_id = data.aws_vpc.vpc.id
 
+  tags = {
+    Name = "PrivateRT"
   }
+}
 
-# route table association for public
+# Route Table Association for Public Subnet
 resource "aws_route_table_association" "PublicToPublic" {
   subnet_id      = aws_subnet.PublicSubnet.id
   route_table_id = aws_route_table.PublicRT.id
 }
 
-# route table association for private
+# Route Table Association for Private Subnet
 resource "aws_route_table_association" "PrivateToPrivate" {
   subnet_id      = aws_subnet.PrivateSubnet.id
   route_table_id = aws_route_table.PrivateRT.id
 }
 
-
+# Route in Public Route Table to IGW
 resource "aws_route" "RouteInPublicRT_TO_IGW" {
-  route_table_id            = aws_route_table.PublicRT.id
-  destination_cidr_block    = "0.0.0.0/0"
- gateway_id                 = data.aws_nat_gateway.nat.id
-  depends_on                = [aws_route_table.PublicRT]
-
+  route_table_id         = aws_route_table.PublicRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+  depends_on             = [aws_route_table.PublicRT]
 }
 
+# NAT Gateway Data Source
+data "aws_nat_gateway" "nat" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+  most_recent = true
+}
+
+# Route in Private Route Table to NAT Gateway
 resource "aws_route" "RouteInPrivateRT_TO_NATGW" {
-  route_table_id            = aws_route_table.PrivateRT.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = data.aws_nat_gateway.nat.id
-  depends_on                = [aws_route_table.PrivateRT]
+  route_table_id         = aws_route_table.PrivateRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = data.aws_nat_gateway.nat.id
+  depends_on             = [aws_route_table.PrivateRT]
 }
 
-#security group
+# Security Group
 resource "aws_security_group" "SG" {
   name        = "SG"
-  description = " SG to alllow traffic from the VPC"
-  vpc_id = data.aws_vpc.vpc.id
-  depends_on = [
-    data.aws_vpc.vpc
-  ]
+  description = "SG to allow traffic from the VPC"
+  vpc_id      = data.aws_vpc.vpc.id
 
- 
-#outbound access
+  # Outbound Access
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-#creating zip of lambda file
+# Creating Zip of Lambda File
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda.py"
   output_path = "lambda.zip"
 }
 
-#lambda function configuration
+# Lambda Function Configuration
 resource "aws_lambda_function" "lambda_handler" {
-  filename      = data.archive_file.lambda.output_path
-  function_name = "lambda_handler"
-  role          = data.aws_iam_role.lambda.arn
-  handler       = "lambda.lambda_handler"
-  runtime       = "python3.9"
+  filename         = data.archive_file.lambda.output_path
+  function_name    = "lambda_handler"
+  role             = data.aws_iam_role.lambda.arn
+  handler          = "lambda.lambda_handler"
+  runtime          = "python3.9"
   source_code_hash = data.archive_file.lambda.output_base64sha256
-
 }
-
-
-
-
-
-
