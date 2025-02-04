ee3cac662106921c8a2bdf94d19760073f2aaa83
Prathamesh Lawand <prathameshl@prescient.in>
2025-02-04 19:41:51 +0530

    updated24

--- main.tf
+++ main.tf
@@ -1,30 +1,62 @@
 # Public Subnet
 resource "aws_subnet" "PublicSubnet" {
   vpc_id     = data.aws_vpc.vpc.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "PublicSubnet"
   }
 }
 
-# Route Table for Public Subnet (Modify route for public access)
+# Private Subnet
+resource "aws_subnet" "PrivateSubnet" {
+  vpc_id     = data.aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+# Route Tables
 resource "aws_route_table" "PublicRT" {
   vpc_id = data.aws_vpc.vpc.id
 }
 
-# Adding default route for Public Subnet
-resource "aws_route" "PublicRoute" {
-  route_table_id         = aws_route_table.PublicRT.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = data.aws_internet_gateway.igw.id  # Ensure you have an Internet Gateway
-
-  depends_on = [
-    aws_subnet.PublicSubnet
-  ]
+resource "aws_route_table" "PrivateRT" {
+  vpc_id = data.aws_vpc.vpc.id
 }
 
-# Security Group (Adjust the rules according to your requirements)
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "PublicToPublic" {
+  subnet_id      = aws_subnet.PublicSubnet.id
+  route_table_id = aws_route_table.PublicRT.id
+  depends_on     = [aws_subnet.PublicSubnet, aws_route_table.PublicRT]
+}
+
+# Associate Private Subnet with Private Route Table 
+resource "aws_route_table_association" "PrivateToPrivate" {
+  subnet_id      = aws_subnet.PrivateSubnet.id
+  route_table_id = aws_route_table.PrivateRT.id
+  depends_on     = [aws_subnet.PrivateSubnet, aws_route_table.PrivateRT]
+}
+
+# Create Route in Public Route Table 
+resource "aws_route" "RouteInPublicRT_TO_IGW" {
+  route_table_id         = aws_route_table.PublicRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = data.aws_nat_gateway.nat.id 
+  depends_on             = [aws_route_table.PublicRT]
+}
+
+# Create Route in Private Route Table to NAT Gateway
+resource "aws_route" "RouteInPrivateRT_TO_NATGW" {
+  route_table_id         = aws_route_table.PrivateRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = data.aws_nat_gateway.nat.id
+}
+
+# Security Group
 resource "aws_security_group" "SG" {
   name        = "SG"
   description = "Security Group to allow traffic from the VPC"
@@ -41,11 +73,11 @@ resource "aws_security_group" "SG" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Adjust according to security requirements
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Lambda Function Setup (No Route Deletion)
+# Lambda function setup
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda.py"
@@ -61,7 +93,7 @@ resource "aws_lambda_function" "lambda_handler" {
   source_code_hash = data.archive_file.lambda.output_base64sha256
 }
 
-# Output Subnet IDs
+# Output Subnet ID
 output "subnet_id" {
-  value = aws_subnet.PublicSubnet.id
+  value = aws_subnet.PrivateSubnet.id
 }
